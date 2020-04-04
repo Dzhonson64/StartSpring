57ad5ead4fd61b31a00e44d8e10ee11e063c7800
Kirill <kirill.chugunov0@gmail.com>
2020-04-04 22:32:03 +0400

    integration "Dependency Injection (DI)" part 2

--- src/main/java/ru/kirill/springcourse/MusicPlayer.java
+++ src/main/java/ru/kirill/springcourse/MusicPlayer.java
@@ -1,10 +1,40 @@
 package ru.kirill.springcourse;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     // Inversion Control (IoC)
     public MusicPlayer(Music music) {
+        this.music = music;
+    }
+
+    /* В старых версия Spring его у нас только сеттер принимает значения, то дефолтный конструктор явно определяем */
+    public MusicPlayer() {
+    }
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
