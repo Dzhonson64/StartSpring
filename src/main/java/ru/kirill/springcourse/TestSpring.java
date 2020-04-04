1e05855ad45f7850d4594edda6fb4063276aa13a
Kirill <kirill.chugunov0@gmail.com>
2020-04-04 21:48:11 +0400

    integration "Dependency Injection (DI)"

--- src/main/java/ru/kirill/springcourse/TestSpring.java
+++ src/main/java/ru/kirill/springcourse/TestSpring.java
@@ -11,10 +11,11 @@ public class TestSpring {
         );
 
         /* Получаем бин (объект), где указываем перым параметром  id из AppContext, вторым класс, который будет возвращён*/
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         /* обязательно закрываем AppContext */
